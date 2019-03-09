2b2eca38ad644c6ad14ec8c7f095e5340f4c7dc8
RaistlinD <Johnd0712@gmail.com>
2019-03-09 20:10:47 +0800

    ok

--- fisher.py
+++ fisher.py
@@ -3,9 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hello/')
+@app.route('/hello')
 def hello():
     return 'hello , flask'
 
 
-app.run()
+def helloo():
+    return 'hello , flask'
+
+
+# app.add_url_rule('/hello', view_func=hello)
+
+# 生产环境nginx+uwsgi
+if __name__ == '__main__':
+    app.run()
