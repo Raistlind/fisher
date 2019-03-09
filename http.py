9b353e8cf3ffa6aa87459cdae75cbd0cc679b0dd
RaistlinD <Johnd0712@gmail.com>
2019-03-09 23:03:00 +0800

    add call url funcdion

--- http.py
+++ http.py
@@ -13,7 +13,10 @@
 # import lib
 import requests
 
+
 class HTTP:
     def get(self, url, return_json=True):
         r = requests.get(url)
-        return r
+        if r.status_code != 200:
+            return {} if return_json else ''
+        return r.json() if return_json else r.text()
