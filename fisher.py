f36b52943ddb1d3d671a4fc11a6abfec88e28e54
RaistlinD <Johnd0712@gmail.com>
2019-03-09 21:41:27 +0800

    ok

--- fisher.py
+++ fisher.py
@@ -6,14 +6,30 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello():
     headers = {
-        'content-type': 'text/plain'
+        'content-type': 'text/plain',
+        'location': 'http://www.bing.com'
     }
     response = make_response('<html><html>', 404)
     response.headers = headers
     return response
 
 
-def helloo():
+@app.route('/book/search/<q>/<page>')
+def search(q, page):
+    """
+        q:普通关键字、ISBN
+        page:
+    :return:
+    """
+    isbn_or_key = 'key'
+    if len(q) == 13 and q.isdigit():
+        isbn_or_key = 'isbn'
+
+    short_q = q.replace('-', '')
+
+    if '-' in q and len(short_q) == 10 and short_q.isdigit:
+        isbn_or_key = 'isbn'
+
     return 'hello , flask'
 
 
