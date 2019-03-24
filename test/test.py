5ef863cb5523c58a16a74c01e82c9112fd01f202
RaistlinD <Johnd0712@gmail.com>
2019-03-24 17:38:16 +0800

    ok

---
+++ test/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   test.py    
+@Contact :   Johnd0712@hotmail.com
+@License :   (C)Copyright 2017-2018, Krynn.cn
+
+@Modify Time         @Author      @Version    @Desciption
+-----------------    ---------    --------    -----------
+2019-03-24 17:28      Raistlind    1.0         None
+"""
+
+# import lib
+from flask import Flask, current_app
+
+app = Flask(__name__)
+
+a = current_app
+
+if __name__ == '__main__':
+    pass
