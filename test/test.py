9488c4aa60d85b6b13b88f99f337696640474267
RaistlinD <Johnd0712@gmail.com>
2019-03-24 21:26:16 +0800

    ok

--- test/test.py
+++ test/test.py
@@ -14,8 +14,15 @@
 from flask import Flask, current_app
 
 app = Flask(__name__)
+# ctx = app.app_context()
+# ctx.push()
+# a = current_app
+# d = current_app.config['DEBUG']
+# ctx.pop()
 
-a = current_app
+with app.app_context():
+    a = current_app
+    d = current_app.config['DEBUG']
 
 if __name__ == '__main__':
     pass
