6e51fa02a83c94a676051cf55fc5b1fc26400fa8
RaistlinD <Johnd0712@gmail.com>
2019-03-27 22:50:15 +0800

    ok

---
+++ app/view_models/book.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   book.py   
+@Contact :   Johnd0712@hotmail.com
+@License :   (C)Copyright 2017-2018, Krynn.cn
+
+@Modify Time         @Author      @Version    @Desciption
+-----------------    ---------    --------    -----------
+2019-03-27 22:28      RaistlinD    1.0         None
+"""
+
+
+# import lib
+
+
+class BookViewModel:
+    @classmethod
+    def package_single(cls, data, keyword):
+        returned = {
+            'books': [],
+            'total': 0,
+            'keyword': keyword
+        }
+        if data:
+            returned['total'] = 1
+            returned['books'] = [cls.__cut_book_data(data)]
+        return returned
+
+    @classmethod
+    def package_collection(cls, keyword):
+        pass
+
+    @classmethod
+    def __cut_book_data(cls, data):
+        book = {
+            'title': data['title'],
+            'publisher': data['publisher'],
+            'pages': data['pages'],
+            'author': '、'.join(data['author']),
+            'price': data['price'],
+            'summary': data['summary'],
+            'image': data['image']
+        }
+        return book
+
+
+if __name__ == '__main__':
+    pass
