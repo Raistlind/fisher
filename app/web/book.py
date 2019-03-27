4be4db6d658692f31ba626ef39349f1050c7622a
RaistlinD <Johnd0712@gmail.com>
2019-03-27 23:35:48 +0800

    ok

--- app/web/book.py
+++ app/web/book.py
@@ -14,6 +14,7 @@
 from flask import jsonify, request
 
 from app.forms.book import SearchForm
+from app.view_models.book import BookViewModel
 from . import web
 from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
@@ -36,12 +37,15 @@ def search():
         isbn_or_key = is_isbn_or_key(q)
         if isbn_or_key == 'isbn':
             result = YuShuBook.search_by_isbn(q)
+            result = BookViewModel.package_single(result, q)
         else:
             result = YuShuBook.search_by_keyword(q, page)
+            result = BookViewModel.package_collection(result, q)
         return jsonify(result)
         # return json.dumps(result), 200, {'content-type': 'application/json'}
     else:
         return jsonify(form.errors)
+
 
 #
 if __name__ == '__main__':
