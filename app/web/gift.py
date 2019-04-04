44db6cb61289664f21079777f1ab8ef6495d14ef
RaistlinD <Johnd0712@gmail.com>
2019-04-04 23:54:21 +0800

    ok

--- app/web/gift.py
+++ app/web/gift.py
@@ -11,13 +11,12 @@
 """
 
 # import lib
-from flask import current_app
+from flask import current_app, flash
 from flask_login import login_required, current_user
 
 from app import db
 from app.models.gift import Gift
 from . import web
-
 
 
 @web.route('/my/gifts')
@@ -29,19 +28,20 @@ def my_gifts():
 @web.route('/gifts/book/<isbn>')
 @login_required
 def save_to_gifts(isbn):
-    gift = Gift()
-    gift.isbn = isbn
-    gift.uid = current_user.id
-    current_user.beans += current_app.config['BEANS_UPLOAD_ONE_BOOK']
-    db.session.add(gift)
-    db.session.commit()
-
-    pass
+    if current_user.can_save_to_list(isbn):
+        try:
+            gift = Gift()
+            gift.isbn = isbn
+            gift.uid = current_user.id
+            current_user.beans += current_app.config['BEANS_UPLOAD_ONE_BOOK']
+            db.session.add(gift)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+    else:
+        flash('这本书已添加至你的赠送清单或者已存在于你的心愿清单，请不要重复添加')
 
 
 @web.route('/gifts/<gid>/redraw')
 def redraw_from_gifts(gid):
     pass
-
-
-
