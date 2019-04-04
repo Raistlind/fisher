6639d2bd290db7da6199519fa0b6d4ec7940842a
RaistlinD <Johnd0712@gmail.com>
2019-04-04 23:34:29 +0800

    ok

--- app/models/user.py
+++ app/models/user.py
@@ -16,7 +16,9 @@ from sqlalchemy import Column, Integer, String, Boolean, Float
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import login_manager
+from app.libs.helper import is_isbn_or_key
 from app.models.base import Base
+from app.spider.yushu_book import YuShuBook
 
 
 class User(Base, UserMixin):
@@ -42,6 +44,14 @@ class User(Base, UserMixin):
 
     def check_password(self, raw):
         return check_password_hash(self._password, raw)
+
+    def can_save_to_list(self, isbn):
+        if is_isbn_or_key(isbn) != 'isbn':
+            return False
+        yushu_book = YuShuBook()
+        yushu_book.search_by_isbn(isbn)
+        if not yushu_book.first:
+            return False
 
     # def get_id(self):
     #     """
