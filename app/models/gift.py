f8c99e950f7b1e67cc8c2ed2551a0f6a53cb00f1
RaistlinD <Johnd0712@gmail.com>
2019-04-05 23:09:53 +0800

    ok

--- app/models/gift.py
+++ app/models/gift.py
@@ -12,10 +12,11 @@
 
 # import lib
 from flask import current_app
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc, func
 from sqlalchemy.orm import relationship
 
-from app.models.base import Base
+from app.models.base import Base, db
+
 from app.spider.yushu_book import YuShuBook
 
 
@@ -27,6 +28,20 @@ class Gift(Base):
     # bid = Column(Integer, ForeignKey('book.id'))
     isbn = Column(String(15), nullable=False)
     launched = Column(Boolean, default=False)
+
+    @classmethod
+    def get_user_gifts(cls, uid):
+        gifts = Gift.query.filter_by(uid=uid, launched=False).order_by(desc(Gift.create_time)).all()
+        return gifts
+
+    @classmethod
+    def get_wish_counts(cls, isbn_list):
+        from app.models.wish import Wish
+        count_list = db.session.query(func.count(Wish.id), Wish.isbn).filter(
+            Wish.launched == False, Wish.isbn.in_(isbn_list), Wish.status == 1).group_by(
+            Wish.isbn).all()
+        count_list = [{'count': w[0], 'isbn': w[1]} for w in count_list]
+        return count_list
 
     @property
     def book(self):
--- app/models/wish.py
+++ app/models/wish.py
@@ -11,10 +11,12 @@
 """
 
 # import lib
-from sqlalchemy import Column, Integer, ForeignKey, String, Boolean
+from sqlalchemy import Column, Integer, ForeignKey, String, Boolean, desc, func
 from sqlalchemy.orm import relationship
 
-from app.models.base import Base
+from app.models.base import Base, db
+
+from app.spider.yushu_book import YuShuBook
 
 
 class Wish(Base):
@@ -26,6 +28,22 @@ class Wish(Base):
     isbn = Column(String(15), nullable=False)
     launched = Column(Boolean, default=False)
 
+    @classmethod
+    def get_user_wishes(cls, uid):
+        wishes = Wish.query.filter_by(uid=uid, launched=False).order_by(desc(Wish.create_time)).all()
+        return wishes
 
-if __name__ == '__main__':
-    pass
+    @classmethod
+    def get_gifts_counts(cls, isbn_list):
+        from app.models.gift import Gift
+        count_list = db.session.query(func.count(Gift.id), Gift.isbn).filter(
+            Gift.launched == False, Gift.isbn.in_(isbn_list), Gift.status == 1).group_by(
+            Gift.isbn).all()
+        count_list = [{'count': g[0], 'isbn': g[1]} for g in count_list]
+        return count_list
+
+    @property
+    def book(self):
+        yushu_book = YuShuBook()
+        yushu_book.search_by_isbn(self.isbn)
+        return yushu_book.first
