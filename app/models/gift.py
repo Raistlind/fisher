cc55239d10c67de17a14d33e53920fdd75d3f56e
RaistlinD <Johnd0712@gmail.com>
2019-04-05 20:11:59 +0800

    ok

--- app/models/gift.py
+++ app/models/gift.py
@@ -12,10 +12,11 @@
 
 # import lib
 from flask import current_app
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc
 from sqlalchemy.orm import relationship
 
 from app.models.base import Base
+from app.spider.yushu_book import YuShuBook
 
 
 class Gift(Base):
@@ -27,10 +28,16 @@ class Gift(Base):
     isbn = Column(String(15), nullable=False)
     launched = Column(Boolean, default=False)
 
+    @property
+    def book(self):
+        yushu_book = YuShuBook()
+        yushu_book.search_by_isbn(self.isbn)
+        return yushu_book.first
+
     @classmethod
     def recent(cls):
         recent_gift = Gift.query.filter_by(launched=False).group_by(Gift.isbn).order_by(
-            Gift.create_datetime).limit(current_app.config['RECENT_BOOK_COUNT']).distinct().all()
+            desc(Gift.create_time)).limit(current_app.config['RECENT_BOOK_COUNT']).distinct().all()
         return recent_gift
 
 
